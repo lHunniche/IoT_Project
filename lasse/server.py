b9a54ea3e59d6e24f4bf85e3f324d650c0c4d845
Hunniche <lklev16@student.sdu.dk>
2020-03-26 14:29:10 +0100

    Update server.py

--- lasse/server.py
+++ lasse/server.py
@@ -20,7 +20,7 @@ def init_board():
     new_board = Board(board_id)
 
     if board_dict.get(board_id) is not None:
-        return board_id + " already initted."
+        return str(board_id) + " already initted."
 
     board_dict[board_id] = new_board
     return str(board_id) + " initted."
